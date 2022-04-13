826de2c7f793b68386483733d1e6e05c2b790853
j-prt <94670559+j-prt@users.noreply.github.com>
2022-04-12 18:25:47 -0700

    Rework logic around catching game-winning scenarios, add logic for draw scenario, more or less flesh out the game and implement a basic game loop

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 """Defines classes to be used in m,n,k-style game."""
 
+
 VALS = ["_", "X", "O"]
 COLS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"]
 
@@ -7,17 +8,23 @@ COLS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O
 class Board:
     """Maintains and draws the state of the game board as well as win/lose conditions"""
 
-    def __init__(self, m, n, k):
+    def __init__(self, m, n, k=4):
         """m denotes board width (rows), n denotes height (columns), k denotes required streak for a win"""
         self.m = m
         self.n = n
         self.k = k
         self.board = [[0 for row in range(self.m)] for col in list(range(self.n))]
+        self.game_on = False
         self.players = []
+        self.moves = 0
+        self.max_moves = m * n
         
+    def new_board(self):
+        self.moves = 0
+        self.board = [[0 for row in range(self.m)] for col in list(range(self.n))]
 
     def draw_board(self):
-        """Generates game board based on passed parameters"""
+        """Draws the game board based on board data"""
         board_drawn = "    "
         for n in range(len(self.board[0])):
             board_drawn += f" {COLS[n]}  "   
@@ -28,45 +35,159 @@ class Board:
             board_drawn += "|"
         print(board_drawn)
 
-    def game_end(self):
-        """Identifies if the game is in a winning or draw state"""
-        pass
+    def win(self, player):
+        """Identifies if the game is in a winning state"""
 
-    def update(self, player):
-        """Updates the board with players' move, calls draw_board and game_end"""
-        if len(player.moves) < self.k:
-            pass
+        #Original function for checking a diagonal win scenario
+        # for row_index, row in enumerate(self.board):
+        #     for col_index, num in enumerate(row):
+        #         try:
+        #             if num == 0:
+        #                 continue
+        #             elif self.board[row_index+1][col_index+1] == num and self.board[row_index-1][col_index-1] == num:
+        #                 if row_index-1 < 0 or col_index-1 < 0:
+        #                     continue
+        #                 print('diagonal1')
+        #                 player.score += 1
+        #                 return True
+        #             elif self.board[row_index-1][col_index+1] == num and self.board[row_index+1][col_index-1] == num:
+        #                 if row_index-1 < 0 or col_index-1 < 0:
+        #                     continue
+        #                 print('diagonal2')
+        #                 player.score += 1
+        #                 return True
+        #         except:
+        #             continue
+                
+        #Updated diagonal win checker
+        for row_index, row in enumerate(self.board):
+            for item_index, item in enumerate(row[:-self.k+1]):
+                count_to_kd_pos = 1
+                count_to_kd_neg = 1
+                if item != player.player:
+                    continue
+                item_index_pos = item_index
+                item_index_neg = item_index
+                row_index_pos = row_index
+                row_index_neg = row_index
+                for i in range(self.k):
+                    try:
+                        if item == self.board[row_index_pos+1][item_index_pos+1]:
+                            count_to_kd_pos += 1
+                            item_index_pos += 1
+                            row_index_pos += 1
+                    except:
+                        pass
+                    try:
+                        if item == self.board[row_index_neg-1][item_index_neg+1]:
+                            item_index_neg += 1
+                            row_index_neg -= 1
+                            count_to_kd_neg += 1
+                    except:
+                        pass
+                    if count_to_kd_pos == self.k or count_to_kd_neg == self.k:
+                        player.score += 1
+                        return True
+                    pass
+        
+
+        #Check horizontal win
+        for row in self.board:
+            count_to_k = 1
+            for i in range(self.m-1):
+                if row[i] == row[i+1] and row[i] == player.player:
+                    count_to_k += 1
+                else:
+                    count_to_k = 1
+                if count_to_k == self.k:
+                    player.score += 1
+                    return True
+                else:
+                    continue
+
+
+        #Check vertical win
+        for alpha, column in player.moves.items():
+            column = sorted(list(column))
+            count_to_kv = 1
+            for item in column:
+                try:
+                    if item + 1 == column[column.index(item)+1]:
+                        count_to_kv +=1
+                    else:
+                        count_to_kv = 1
+                except:
+                    count_to_kv = 1
+                if count_to_kv == self.k:
+                    player.score += 1
+                    return True
+            return False
+
+    def draw(self):
+        if self.moves == self.max_moves:
+            return True
         else:
-            pass
-        pass
+            return False
+
+    def game_end(self):
+        """Updates the board with players' move, calls draw_board and game_end"""
+        self.players[0].moves = {}
+        self.players[1].moves = {}
+        self.moves = 0
+        self.game_on = False
+        self.new_board()
+
+
 
 
 class Player:
     """Contains basic player information and actions"""
 
-    def __init__(self, player=int, game_board=None):
+    def __init__(self, name=str, player=int, game_board=None):
         self.player = player
-        self.moves = []
+        self.name = name
+        self.moves = {}
         self.score = 0
         self.game_board = game_board
         self.game_board.players.append(self)
 
-    def place(self, coord):
+    def place(self):
         """Add a marker to the board. Coordinate received as tuple from user input"""
-        alpha, num = list(coord)[0], int(list(coord)[1])-1
-        self.game_board.board[num][COLS.index(alpha)] = self.player
-        self.moves.append(coord)
-        self.game_board.update(self)
+        coord = input(f'{self.name}, your move: ').upper()
+        if coord == "FORFEIT":
+            self.forfeit()
+            return None
+
+        try:
+            alpha, num = coord[0], int(coord[1:])-1
+            while self.game_board.board[num][COLS.index(alpha)] != 0 or len(coord) >3:
+                coord = input('Please play a valid move: ').upper()
+                alpha, num = list(coord)[0], int(list(coord)[1])-1
+            self.game_board.board[num][COLS.index(alpha)] = self.player
+            self.game_board.moves +=1
+            self.game_board.draw_board()
+            try:
+                self.moves[alpha] += [num]
+            except:
+                self.moves[alpha] = [num]
+        except:
+            input('Invalid entry... ')
+            self.place()
+        
 
     def forfeit(self):
         """Concede the game"""
+        input(f"{self.name} has forfeitted the game.")
+        self.game_board.players[self.player-2].score += 1
+        self.game_board.game_end()
 
+#TODO - code the AI opponent
 class Com(Player):
     """AI ("COM") player to play against a player in single-player mode"""
 
     def __init__(self):
         super.__init__()
 
-    def move(self):
+    def place(self):
         #coordinate should be based on picking next available adjacency to longest streak
         pass
--- main.py
+++ main.py
@@ -1,39 +1,89 @@
+import graphics, os, re
 from classes import Board, Player
 
+#Function definitions
 
-# VALS = ["_", "X", "O"]
-# COLS = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O"]
-# ROWS = iter(list(range(1, 16)))
-# board = [[0 for n in range(3)] for i in list(range(3))]
+def clear():
+    """Clears terminal. To be used between games"""
 
-# board_drawn = "    "
-# for n in range(len(board[0])):
-#     board_drawn += f" {COLS[n]}  "   
-# for row in board:
-#     board_drawn += f"\n{next(ROWS):2} "
-#     for item in row:   
-#         board_drawn += f"| {VALS[item]} "
-#     board_drawn += "|"
+    os.system('cls' if os.name=='nt' else 'clear')
 
+def start_game():
+    """Prompts user to input desired board size and streak length and craft an m,n,k style game of their liking"""
 
+    print(graphics.title)
+    print('\n\n')
+    game_selection = input("Welcome to Tic-Tac-Oh! What size board do you want? Width x Height: ")
+    if game_selection == 'quit':
+        quit()
 
+    dimensions = [int(n) for n in re.split('\D', game_selection) if n.isdigit()]
+    if len(dimensions) != 2:
+        dimensions = [int(n) for n in re.split('\D', input("Please input two values ")) if n.isdigit()]
+    while dimensions[0] not in list(range(3, 16)) or dimensions[1] not in (range(3, 16)):
+        dimensions = [int(n) for n in re.split('\D', input("Please input values from 3 to 15 ")) if n.isdigit()]
+    select_k = int(input("Streak length? "))
+    while select_k not in list(range(3, 16)):
+        select_k = int(input("Please input a number from 3 to 15 "))
+    return Board(dimensions[0], dimensions[1], select_k)
 
+def new_player(board, x_o):
+    """Instantiate a player"""
 
-# print(board[0] == board)
-# print
+    name = input(f"Input Player {x_o} name: " )
+    return Player(name, x_o, board)
 
-board = Board(3,3,3)
-board.draw_board()
+# Call functions to create board and player,
+#  set counter toggle variable for turn switching 
+# and set game_engine variable to control the main game loop
 
-p1 = Player(1, board)
+board = start_game()
+p1 = new_player(board, 1)
+p2 = new_player(board, 2)
+game_engine = True
+counter = 0
 
-p1.place("A1")
+while game_engine == True:
 
-board.draw_board()
+    # Set game_on variable to control the round loop, 
+    # clear the title screen and print a new game board
 
-#check three things: 
-#one - the horizontal (within the rows)
-#two - the vertical (same index across rows)
-#three - the diagonal (sequential indices across rows)
-#only needs to check current move  
-#track movelist 
+    board.game_on = True
+    clear()
+    print(graphics.battle)
+    board.draw_board()
+    while board.game_on:
+
+        # Toggles player turns using bitwise NOT
+        counter = ~counter
+        if counter:
+            p1.place()
+        else:
+            p2.place()
+
+        # Checks winning conditions for each player,
+        # checks if board is in draw state,
+        # and prints game end flavour text
+
+        if board.win(p1):
+            print(f'{p1.name} is the... \n\n\n {graphics.winner}')
+            board.game_end()
+        elif board.win(p2):
+            print(f'{p2.name} is the... \n\n\n {graphics.winner}')
+            board.game_end()
+        if board.draw():
+            print(f'{graphics.draw}\n\nWell.. isn\'t that disappointing?')
+            board.game_end()
+
+    # Prompts players to play again or quit. 
+    # Final score is displayed on quit and is a mystery until then!
+
+    again = input("Keep playing? Y/N ").lower()
+    while again not in ['y', 'n']:
+        again = input("Keep playing? Y/N ").lower()
+    if again == 'y':
+        continue
+    elif again == 'n':
+        game_engine == False
+        input(f"Score: \n{p1.name}: {p1.score} \n{p2.name}: {p2.score}\n\n\nThanks for playing!")
+        quit()
